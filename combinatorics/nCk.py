0ee2ead98b7dae6e2977ab1b5821d4e07629d670
akhterrehan983 <56243095+akhterrehan983@users.noreply.github.com>
2022-08-27 17:48:50 +0530

    Update nCk.py

--- combinatorics/nCk.py
+++ combinatorics/nCk.py
@@ -1,36 +1,70 @@
-def binomialCoeff(n, r):
-     
-    if (r > n):
-        return 0
-         
-    
-    inv = [0 for i in range(r + 1)]
-    inv[0] = 1;
-    if(r+1>=2)
-    inv[1] = 1;
- 
-    # Getting the modular inversion
-    # for all the numbers
-    # from 2 to r with respect to m
-    # here m = 1000000007
-    for i in range(2, r + 1):
-        inv[i] = m - (m // i) * inv[m % i] % m
- 
-    ans = 1
- 
-    # for 1/(r!) part
-    for i in range(2, r + 1):
-        ans = ((ans % m) * (inv[i] % m)) % m
- 
-    # for (n)*(n-1)*(n-2)*...*(n-r+1) part
-    for i in range(n, n - r, -1):
-        ans = ((ans % m) * (i % m)) % m
-         
-    return ans
- 
-# Driver code
-n = 5
-r = 2
-m = 1000000007
-print("Value of C(" ,n , ", " , r ,
-      ") is ",binomialCoeff(n, r, m))
+# Python3 program to answer queries
+# of nCr in O(1) time.
+N = 1000001
+
+# array to store inverse of 1 to N
+factorialNumInverse = [None] * (N + 1)
+
+# array to precompute inverse of 1! to N!
+naturalNumInverse = [None] * (N + 1)
+
+# array to store factorial of
+# first N numbers
+fact = [None] * (N + 1)
+
+# Function to precompute inverse of numbers
+def InverseofNumber(p):
+	naturalNumInverse[0] = naturalNumInverse[1] = 1
+	for i in range(2, N + 1, 1):
+		naturalNumInverse[i] = (naturalNumInverse[p % i] *
+								(p - int(p / i)) % p)
+
+# Function to precompute inverse
+# of factorials
+def InverseofFactorial(p):
+	factorialNumInverse[0] = factorialNumInverse[1] = 1
+
+	# precompute inverse of natural numbers
+	for i in range(2, N + 1, 1):
+		factorialNumInverse[i] = (naturalNumInverse[i] *
+								factorialNumInverse[i - 1]) % p
+
+# Function to calculate factorial of 1 to N
+def factorial(p):
+	fact[0] = 1
+
+	# precompute factorials
+	for i in range(1, N + 1):
+		fact[i] = (fact[i - 1] * i) % p
+
+# Function to return nCr % p in O(1) time
+def Binomial(N, R, p):
+	
+	# n C r = n!*inverse(r!)*inverse((n-r)!)
+	ans = ((fact[N] * factorialNumInverse[R])% p *
+					factorialNumInverse[N - R])% p
+	return ans
+
+# Driver Code
+if __name__ == '__main__':
+	
+	# Calling functions to precompute the
+	# required arrays which will be required
+	# to answer every query in O(1)
+	p = 1000000007
+	InverseofNumber(p)
+	InverseofFactorial(p)
+	factorial(p)
+
+	# 1st query
+	N = 15
+	R = 4
+	print(Binomial(N, R, p))
+
+	# 2nd query
+	N = 20
+	R = 3
+	print(Binomial(N, R, p))
+
+# This code is contributed by
+# Surendra_Gangwar
