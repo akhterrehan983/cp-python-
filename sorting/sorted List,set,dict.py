35fecf2709a678586537b287625085f9a8f8c7ec
akhterrehan983 <56243095+akhterrehan983@users.noreply.github.com>
2022-09-03 23:22:34 +0530

    Update sorted List,set,dict.py

--- sorting/sorted List,set,dict.py
+++ sorting/sorted List,set,dict.py
@@ -1,1 +1,9 @@
+  from sortedcontainers import SortedList, SortedSet, SortedDict
 
+  sorted_list = SortedList([1, 2, 10, 4])
+  sorted_list.add(-1)
+  sorted_list.discard(2)
+  sorted_list.update([3,20])
+  print(sorted_list)
+  sorted_list.clear()
+  print(sorted_list)
